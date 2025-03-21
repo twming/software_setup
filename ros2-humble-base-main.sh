39c696cdda4ed6bfa1c2cf7cc9fc7addd864b853
twming <tanwoeiming@gmail.com>
2025-03-21 22:37:37 +0800

    Update ros2-humble-base-main.sh

--- ros2-humble-base-main.sh
+++ ros2-humble-base-main.sh
@@ -33,7 +33,7 @@ printf '\033[33m%s\033[m\n' "|           Get ROS 2 Humble key                 |"
 printf '\033[33m%s\033[m\n' "=================================================="
 
 sudo apt install -y software-properties-common 
-sudo add-apt-repository universe
+sudo add-apt-repository -y universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
